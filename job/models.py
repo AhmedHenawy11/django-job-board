664689196c60b922e664bf09ebe124bdc1690b27
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2023-12-16 00:11:17 +0200

    still working

--- job/models.py
+++ job/models.py
@@ -13,6 +13,7 @@ GENDER = (
 
 class Job (models.Model): #table
     title = models.CharField(max_length=100)
+    category = models.ForeignKey('category', on_delete=models.CASCADE)
     job_type = models.CharField(max_length=20, choices=JOB_TYPE)
     description = models.TextField(max_length=1000)
     published_at = models.DateField(auto_now=True)
@@ -20,6 +21,13 @@ class Job (models.Model): #table
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     gender = models.CharField(max_length=20, choices=GENDER)
+    
 
     def __str__(self):
         return self.title
+    
+class category (models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
